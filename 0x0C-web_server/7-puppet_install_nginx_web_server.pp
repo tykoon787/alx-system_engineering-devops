28cbe4484b430e714710a4c7bba22cc5a88f4a64
tykoon787 <tykoon787@protonmail.com>
2023-04-03 22:51:49 +0300

    Created Code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,17 +5,17 @@ class { 'nginx':
   manage_repo => true,
 }
 
-$url = "https://www.youtube.com/watch?v=QH2-TGUlwu4"
+$url = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
 $web_01='52.87.215.121'
 nginx::resource::server { $web_01:
-  listen_port => '80',
-  server_name => $web_01,
-  location => {
-    '/' => {
-      'content' => '<html><body><p>"Hello World!</p></body></html>'
+  listen_port       => '80',
+  server_name       => $web_01,
+  location          => {
+    '/'             => {
+      'content'     => '<html><body><p>"Hello World!</p></body></html>'
     }
-    '/redirect_me' => {
-      'return' => '301 $url'
+    '/redirect_me'  => {
+      'return'      => '301 $url'
     }
   }
 }

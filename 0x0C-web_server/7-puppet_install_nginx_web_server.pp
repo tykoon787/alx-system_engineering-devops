35927b8e2eaf09a7da0038f3b4ec1b09e2825bf7
tykoon787 <tykoon787@protonmail.com>
2023-04-03 23:50:51 +0300

    Created Code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,54 @@
 # Setups an nginx server
 
-# Install server
-class { 'nginx':
-  manage_repo => true,
+# Variables
+$doc_root = '/var/www/html'
+$server_ip = '52.87.215.121'
+# Install the server
+## Update
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
 }
 
-$url = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-$web_01='52.87.215.121'
-nginx::resource::server { $web_01:
+## Install the package
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get update']
+}
+
+file { $doc_root:
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644'
+}
+
+file { "${doc_root}/index.html":
+  ensure  => 'present',
+  content => '<html><body><h1>Hello World!</h1></body></html>'
+}
+
+nginx::resource::server {'web-01':
   listen_port => '80',
-  server_name => $web_01,
+  server_name => '$server_ip',
+
   location    => {
     '/'            => {
-      'content'    => '<html><body><p>"Hello World!</p></body></html>'
-    }
+      content => '<html><body><h1>Hello World!</h1></body></html>'
+
+    },
     '/redirect_me' => {
-      'return'     => '301 $url'
-    }
-  }
+      return => '301 https://www.youtube.com/watch?v=QH2-TGUlwu4'
+    },
+  },
 }
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+service { 'nginx':
+  ensure=> running,
+}
+

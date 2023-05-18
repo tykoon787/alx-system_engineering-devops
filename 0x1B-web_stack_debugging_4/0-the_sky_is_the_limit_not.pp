ade05d612db1168f62344d739f670cd486b5e36f
tykoon787 <tykoon787@protonmail.com>
2023-05-18 13:56:30 +0300

    Updated Code

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increases the number of requests that nginx can handle 
+
+exec { 'ngix-conf':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
